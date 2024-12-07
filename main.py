770f6418fcab07c03e302bc003da3c330133b2b8
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2024-12-07 12:13:16 +0100

    added triangle on black screen

--- main.py
+++ main.py
@@ -3,11 +3,16 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
+    Clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,8 +22,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((255, 255, 255))
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = Clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+from circleshape import *
+from constants import *
+
+# Base class for game objects
+class Player(CircleShape):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        # sub-classes must override
+        pygame.draw.polygon(screen,(255,255,255),self.triangle(), 2)
