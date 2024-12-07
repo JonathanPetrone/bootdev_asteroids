b2181a8cac1dfe7f9bf7e4dac46552da2274cdbc
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2024-12-07 12:35:09 +0100

    made it move

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -5,8 +5,6 @@ from constants import *
 # Base class for game objects
 class Player(CircleShape):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
@@ -21,3 +19,14 @@ class Player(CircleShape):
     def draw(self, screen):
         # sub-classes must override
         pygame.draw.polygon(screen,(255,255,255),self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
